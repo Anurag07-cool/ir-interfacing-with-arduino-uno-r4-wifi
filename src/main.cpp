a45070139475ed771d4da77926f049df66c6a3ba
Anurag Chaudhary <anurag.choudhary_cs.aiml24@gla.ac.in>
2026-02-03 10:03:51 +0530

    add code in loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
-#include "Arduino.h"
-#define irPin=2
-
-void setup()
-{
-    pinMode(irPin , INPUT);
+#include <Arduino.h>
+#define irPin 2
+void setup() {
+    pinMode(irPin, INPUT);
 }
 
-void loop()
-{
-
+void loop() {
+    int irvalue = digitalRead(irPin);
+    if (irvalue == LOW) {
+        Serial.println("IR Sensor: No Obstacle");
+    } else {
+        Serial.println("IR Sensor: Obstacle Detected");
+    }
+    delay(500);
 }
